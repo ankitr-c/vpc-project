9c784ac2e09a42c1960f9676a924df249cbb0401
Ankit <ankit.raut@coditas.com>
2023-12-12 22:31:21 +0530

    fixed ip and project name for this project

--- Execution-Script/DockerScript.sh
+++ Execution-Script/DockerScript.sh
@@ -55,7 +55,7 @@ cd "$path"
 sudo docker-compose down && echo "Docker Is Down Now" || echo "Docker Already Down"
 
 # sudo docker rmi staticip-multivm-dockerproject_frontend || echo "Error deleting frontend image"
-sudo docker rmi <staticip-multivm-dockerproject>_frontend || echo "Error deleting frontend image"
+sudo docker rmi vpc-project_frontend || echo "Error deleting frontend image"
 
 sudo docker-compose up -d || echo "error in compose file"
 
